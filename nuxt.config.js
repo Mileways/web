599111ee52a70a527bfe2b31fc47b09439ee4031
Rico Ocepek <rico@ocpk.at>
2020-07-15 17:00:09 +0200

    Added favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,10 +24,11 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'msapplication-TileColor', content: tailwindConfig.theme.extend.colors.primary },
       { name: 'theme-color', content: tailwindConfig.theme.extend.colors.primary }
     ],
     link: [
+
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
