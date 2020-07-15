7ba232ab06309d63b882aeec7b4b8d23a8e499f4
Rico Ocepek <rico@ocpk.at>
2020-07-15 11:36:18 +0200

    Added index page w/ footer and header

---
+++ components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="py-10 mt-10 text-center">
+    <span class="text-gray-400">&copy; {{ currentYear }} Mileways GmbH</span>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+
+  computed: {
+    currentYear() {
+      return (new Date()).getFullYear()
+    }
+  }
+}
+</script>
---
+++ layouts/landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="default">
+    <Header />
+
+    <nuxt class="mt-8" />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/header.vue'
+import Footer from '~/components/footer.vue'
+
+export default {
+  components: { Header, Footer }
+}
+</script>
