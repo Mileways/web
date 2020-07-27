eef347974c7955ae4d10fc47dfcfbae6fd86baf9
Rico Ocepek <rico@ocpk.at>
2020-07-27 15:46:54 +0200

    Fixed meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Mileways | Go Travel' },
       { name: 'msapplication-TileColor', content: tailwindConfig.theme.extend.colors.primary },
       { name: 'theme-color', content: tailwindConfig.theme.extend.colors.primary }
     ],
