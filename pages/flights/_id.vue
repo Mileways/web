e97ead9ea5d9a4cdefa3fc613677fa9b6a4f7fd8
Rico Ocepek <rico@ocpk.at>
2020-07-27 14:46:08 +0200

    Implemented flight view base layout

---
+++ pages/flights/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-11/12 mx-auto max-w-5xl">
+    <div class="bg-white rounded-lg shadow-lg py-10 px-10 mb-10">
+      Title
+    </div>
+
+    <div class="bg-white rounded-lg shadow-lg py-10 px-10 mb-10">
+      Map
+    </div>
+
+    <div class="bg-white rounded-lg shadow-lg py-10 px-10 mb-10">
+      Details
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'FlightView'
+  })
+</script>
+
+<style>
+  body {
+    @apply bg-gray-200;
+  }
+</style>
