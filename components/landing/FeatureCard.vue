29b75d9bfa2c0d09d38ca250477e2301e9dcac6c
Rico Ocepek <rico@ocpk.at>
2020-09-03 15:27:56 +0200

    Implemented feature section

---
+++ components/landing/FeatureCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="bg-gray-200 rounded-xl overflow-hidden mb-16"
+    style="-webkit-column-break-inside: avoid;"
+  >
+    <slot name="cover" />
+
+    <div class="bg-gray-100 p-10 text-left">
+      <h3 class="text-3xl font-bold">
+        {{ title }}
+      </h3>
+
+      <p class="mb-0">
+        <slot />
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'FeatureCard',
+
+    props: {
+      title: { type: String, required: false, default: 'Feature' }
+    }
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -35,12 +35,96 @@
           </div>
         </div>
     </section>
+
+    <section class="text-center container mx-auto">
+      <h2 class="text-4xl font-black mb-5">Features</h2>
+
+      <p class="max-w-3xl inline-block mx-auto text-left text-xl text-gray-700 mb-20">
+        Certe, inquam, pertinax non possim accommodare torquatos nostros? quos dolores eos, qui dolorem. Alii autem, quibus ego cum memoriter, tum etiam erga nos amice et quasi.
+      </p>
+
+      <div style="column-count: 2; column-gap: 4rem;">
+        <FeatureCard title="My Flights">
+          <template v-slot:cover>
+            <img
+                srcset="
+                /images/mockups/my_flights.png,
+                /images/mockups/my_flights_2x.png 2x,
+                /images/mockups/my_flights_3x.png 3x
+              "
+                src="/images/mockups/my_flights.png"
+                class="mx-auto mt-10"
+                alt="My flights mockup"
+            >
+          </template>
+
+          Ut placet, inquam tum dicere exorsus est consecutus? laudem et argumentandum et impetus. Ut placet, inquam tum dicere exorsus est laborum et dolorum fuga et fortibus.
+        </FeatureCard>
+
+        <FeatureCard title="Flight Details">
+          <template v-slot:cover>
+            <img
+                srcset="
+                /images/mockups/flight_details.png,
+                /images/mockups/flight_details_2x.png 2x,
+                /images/mockups/flight_details_3x.png 3x
+              "
+                src="/images/mockups/flight_details.png"
+                class="mx-auto mt-10"
+                alt="Flight details mockup"
+            >
+          </template>
+
+          Ut placet, inquam tum dicere exorsus est consecutus? laudem et argumentandum et impetus. Ut placet, inquam tum dicere exorsus est laborum et dolorum fuga et fortibus.
+        </FeatureCard>
+
+        <div class="pt-56">
+          <FeatureCard title="Friends' Flights">
+            <template v-slot:cover>
+              <img
+                  srcset="
+                  /images/mockups/friends_flights.png,
+                  /images/mockups/friends_flights_2x.png 2x,
+                  /images/mockups/friends_flights_3x.png 3x
+                "
+                  src="/images/mockups/friends_flights.png"
+                  class="mx-auto mt-10"
+                  alt="Friends' flights mockup"
+              >
+            </template>
+
+            Ut placet, inquam tum dicere exorsus est consecutus? laudem et argumentandum et impetus. Ut placet, inquam tum dicere exorsus est laborum et dolorum fuga et fortibus.
+          </FeatureCard>
+        </div>
+
+        <FeatureCard title="Notifications">
+          <template v-slot:cover>
+            <img
+                srcset="
+                /images/mockups/notifications.png,
+                /images/mockups/notifications_2x.png 2x,
+                /images/mockups/notifications_3x.png 3x
+              "
+                src="/images/mockups/notifications.png"
+                class="mx-auto mt-10"
+                alt="Notifications mockup"
+            >
+          </template>
+
+          Ut placet, inquam tum dicere exorsus est consecutus? laudem et argumentandum et impetus. Ut placet, inquam tum dicere exorsus est laborum et dolorum fuga et fortibus.
+        </FeatureCard>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+  import FeatureCard from '../components/landing/FeatureCard'
+
   export default {
-    layout: 'empty'
+    layout: 'empty',
+
+    components: { FeatureCard }
   }
 </script>
 
