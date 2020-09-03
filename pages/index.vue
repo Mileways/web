4cdadcd5cbeea52e2260522ef8688c02e07e62be
Rico Ocepek <rico@ocpk.at>
2020-09-03 11:28:43 +0200

    Implemented welcome-section

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,52 @@
 <template>
-  <div
-    class="teaser-page-container"
-  >
-    <div class="container mx-auto">
-      <img src="/images/logo_text.svg" alt="Mileways Logo" class="mx-auto">
+  <div>
+    <section class="welcome-section">
+        <div class="container mx-auto grid grid-cols-2">
+          <div>
+            <h1 class="text-5xl sm:text-6xl mt-3 font-black text-white">
+              Go Travel.
+            </h1>
 
-      <h1 class="text-5xl sm:text-6xl mt-3 font-black text-white">Flying Soon.</h1>
+            <p class="text-white text-4xl">
+              Everything you need to know about your flights and your friends' flights.
+              <br>
 
-      <div class="sm:flex items-center mt-10">
-        <div class="action-item-wrapper ml-auto">
-          <div class="action-item sm:mr-5 mb-5 sm:mb-0">
-            <TwitterFollowButton />
+              <span class="font-bold">Fast, convenient and beautiful.</span>
+
+              <img
+                src="/images/download.svg"
+                alt="Download on the App Store"
+                class="h-10 mt-5"
+              >
+            </p>
+          </div>
+
+          <div>
+            <img
+              srcset="
+                /images/mockups/main_mockup.png,
+                /images/mockups/main_mockup_2x.png 2x,
+                /images/mockups/main_mockup_3x.png 3x
+              "
+              src="/images/mockups/main_mockup.png"
+              class="ml-auto"
+              alt="Main mockup"
+            >
           </div>
         </div>
-
-        <div class="action-item-wrapper mr-auto">
-          <div class="action-item">
-            <MailChimpSignUpForm class="flex-grow" />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="phone-mockup-wrapper">
-      <div class="container mx-auto mt-20">
-        <img
-            srcset="
-              /images/mockups/horizontal_phone.png,
-              /images/mockups/horizontal_phone_2x.png 2x,
-              /images/mockups/horizontal_phone_3x.png 3x
-            "
-            src="/images/mockups/horizontal_phone.png"
-            class="mx-auto"
-            alt="Horizontal phone"
-        >
-      </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
-  import MailChimpSignUpForm from '../components/teaser/MailchimpSignUpForm'
-  import TwitterFollowButton from '../components/teaser/TwitterFollowButton'
-
   export default {
-    layout: 'empty',
-
-    components: { MailChimpSignUpForm, TwitterFollowButton }
+    layout: 'empty'
   }
 </script>
 
 <style scoped>
-  .teaser-page-container {
-    @apply flex flex-col text-center pt-20 bg-primary;
-  }
-
-  .phone-mockup-wrapper {
-    @apply bg-no-repeat mt-auto;
+  .welcome-section {
+    @apply flex flex-col pt-20 bg-primary bg-no-repeat mt-auto;
 
     background-image: url(/images/clouds.svg);
     background-position: center calc(99.99999% + 1px);
@@ -64,32 +54,16 @@
     background-size: auto 7rem;
   }
 
-  .action-item-wrapper {
-    @apply block;
-  }
-
-  .action-item {
-    @apply inline-flex items-center bg-white text-black border-white border-2 overflow-hidden rounded-lg;
-
-    width: 256px;
-  }
-
   @screen sm {
-    .action-item-wrapper {
-      @apply inline-block;
-    }
-
-    .phone-mockup-wrapper {
+    .welcome-section {
       background-size: auto 10rem;
     }
   }
 
   @screen md {
-    .teaser-page-container {
+    .welcome-section {
       @apply pt-40;
-    }
 
-    .phone-mockup-wrapper {
       background-size: auto 15rem;
     }
   }
