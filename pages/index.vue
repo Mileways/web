72125f7525ea96b88acb7d3cac176f60fbb8e500
Rico Ocepek <rico@ocpk.at>
2020-09-08 11:17:43 +0200

    Implemented story section

--- pages/index.vue
+++ pages/index.vue
@@ -146,6 +146,48 @@
         </div>
       </div>
     </section>
+
+    <section class="container mx-auto my-20">
+      <div class="w-9/12 mx-auto">
+        <h2 class="text-4xl font-black mb-5">Story</h2>
+
+        <p>
+          <span class="font-bold">We've come a long way.</span>
+          Started in 2013, Mileways was first a service for collecting and redeeming miles independently of airlines.
+          We’ve learned it the hard way, that this was not really something people asked for.
+        </p>
+      </div>
+
+      <div class="rounded-xl bg-gray-200 p-10 my-10">
+        <img
+          srcset="
+            /images/old_webpage.png,
+            /images/old_webpage_2x.png 2x,
+            /images/old_webpage_3x.png 3x
+          "
+          src="/images/old_webpage.png"
+          class="mx-auto mt-10"
+          alt="Old webpage"
+        >
+      </div>
+
+      <div class="w-9/12 mx-auto">
+        <p>
+          But ever since we believed, that the need was big for a simple, elegant and fast flight tracker.
+          A place where people would be able to store their past and upcoming flights beautifully,
+          a place where relevant information about flights is being sent ridiculously fast,
+          a place where it’s super easy to share flight information with friends,
+          maybe even without actively sharing it.
+          And most of all, a reliable service which supports all air passengers and people serving the sky during their journeys.
+        </p>
+
+        <p>
+          So we have used all our years of experience in this market,
+          all our knowledge and power to deliver the new version of Mileways.
+          And we hope you enjoy it as much as wo love to work on it.
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
