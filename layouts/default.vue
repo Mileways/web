1d72d9bd377bd66b621eef15d15cf577c4f0ad5f
Rico Ocepek <rico@ocpk.at>
2020-09-09 11:33:10 +0200

    Fixed import paths

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,8 +11,8 @@
 </template>
 
 <script>
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 
 export default {
   components: { Header, Footer }
