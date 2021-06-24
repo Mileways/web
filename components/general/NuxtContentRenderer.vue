3bc28e636033b4ef4fc2809eeefa6af19547377d
Rico Ocepek <rico@ninetyfive.app>
2021-06-24 14:39:52 +0200

    Updated privacy policy

--- components/general/NuxtContentRenderer.vue
+++ components/general/NuxtContentRenderer.vue
@@ -24,19 +24,35 @@ export default Vue.extend({
 
 <style scoped>
 .content-renderer-container::v-deep .nuxt-content h1 {
-  @apply text-4xl font-bold mb-5;
+  @apply text-5xl font-bold mb-5;
 }
 
 .content-renderer-container::v-deep .nuxt-content h2 {
-  @apply text-2xl font-semibold mb-5 mt-10;
+  @apply text-4xl font-semibold mb-5 mt-10;
+}
+
+.content-renderer-container::v-deep .nuxt-content h3 {
+  @apply text-3xl font-semibold mb-5 mt-8;
+}
+
+.content-renderer-container::v-deep .nuxt-content h4 {
+  @apply text-2xl font-semibold mb-5 mt-6;
 }
 
 .content-renderer-container::v-deep .nuxt-content li {
-  @apply list-disc;
+  @apply list-disc mb-3;
+}
+
+.content-renderer-container::v-deep .nuxt-content li p {
+  @apply inline;
 }
 
 .content-renderer-container::v-deep .nuxt-content a {
   @apply text-primary;
+}
+
+.content-renderer-container::v-deep .nuxt-content ul {
+  @apply mb-5;
 }
 
 .content-renderer-container::v-deep .nuxt-content blockquote {
