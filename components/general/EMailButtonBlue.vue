147b427f6ae4b1e44fa4dd5d6479103400c96b86
manueljenni <manuelnoahjenni@outlook.com>
2022-11-19 10:11:56 +0100

    Add Email signup button
    Add signup button that will redirect to a Mailchimp form.
    
    This could probably be done better but I don't fully understand Vue yet.

---
+++ components/general/EMailButtonBlue.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button
+      class="text-white bg-primary px-8 py-4 rounded-lg text-center text-xl w-full hover"
+      @click="openNewWindow"
+    >
+      Get Email updates
+    </button>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'EMailButtonWhite',
+
+    methods: {
+      openNewWindow() {
+        parent.open("http://eepurl.com/g-rWrH");
+      }
+    } 
+  })
+</script>
+
+<style scoped>
+  .hover {
+    transition: all .1s ease-in;
+    border: 1px solid #00a1f2;
+  }
+  .hover:hover {
+    @apply text-primary;
+    background: transparent;
+  }
+</style>
---
+++ components/general/EMailButtonWhite.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button
+      class="text-primary bg-white px-8 py-4 rounded-lg text-center text-xl w-full hover"
+      @click="openNewWindow"
+    >
+      Get Email updates
+    </button>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'EMailButtonWhite',
+
+    methods: {
+      openNewWindow() {
+        parent.open("http://eepurl.com/g-rWrH");
+      }
+    } 
+  })
+</script>
+
+<style scoped>
+  .hover {
+    transition: all .1s ease-in;
+    border: 1px solid white;
+  }
+  .hover:hover {
+    color: white;
+    background: transparent;
+  }
+</style>
