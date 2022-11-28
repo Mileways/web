921fa9ba8a344d89db769f29daaac98d1f4fc16a
Márton Salomváry <marton@salomvary.com>
2022-11-28 12:11:53 +0100

    Add email verification pages
    https://github.com/Mileways/ios_app/issues/645
    https://github.com/Mileways/serverjava/issues/223

---
+++ components/general/LinkButtonBlue.vue
@@ -0,0 +1,25 @@
+<template>
+  <a class="text-white bg-primary px-8 py-3 rounded-lg text-center text-xl w-full hover inline-block">
+    <slot />
+  </a>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LinkButtonBlue'
+})
+</script>
+
+<style scoped>
+.hover {
+  transition: all .1s ease-in;
+  border: 1px solid #00a1f2;
+}
+
+.hover:hover {
+  @apply text-primary;
+  background: transparent;
+}
+</style>
---
+++ pages/email-verified.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen bg-gray-100 p-4 flex flex-row justify-center items-start">
+    <div class="bg-white rounded-lg p-4 text-center max-w-sm flex-grow">
+      <div class="text-4xl mt-10">
+        ✅
+      </div>
+
+      <div style="max-width: 240px" class="mx-auto mt-5">
+        <h2 class="font-bold text-2xl">Email successfully verified</h2>
+
+        <p class="mt-4">Thank you, you can now continue in the app and sign in.</p>
+
+      </div>
+
+      <p class="mt-8">
+        <LinkButtonBlue href="mileways://">Open Mileways</LinkButtonBlue>
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import LinkButtonBlue from "~/components/general/LinkButtonBlue";
+
+export default {
+  components: {LinkButtonBlue}
+}
+</script>
+
+<style scoped>
+</style>
