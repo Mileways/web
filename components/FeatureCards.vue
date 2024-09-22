8ecbca2beaf794709b694cc610a01d314d0bd359
manueljenni <manuelnoahjenni@outlook.com>
2024-09-22 22:36:32 +0200

    Update feature images

--- components/FeatureCards.vue
+++ components/FeatureCards.vue
@@ -4,15 +4,15 @@
     <FeatureCard
       title="Easily track your friends’ and family’s flights."
       description="Your friend’s flight automatically appear on the friends’ tab as they add their flights to Mileways."
-      backgroundImage="/images/feature-box-1.svg" />
+      backgroundImage="/images/feature-box-1.png" />
     <FeatureCard
       title="Richer flight data and additional insights for your flights."
       description="With our own flight data engine, we are able to provide data like checkin desk, inbound plane, and many more."
-      backgroundImage="/images/feature-box-2.svg" />
+      backgroundImage="/images/feature-box-2.png" />
     <FeatureCard
       title="Handcrafted stamps making travel more fun and delightful."
       description="We built Mileways because of problems we faced while travelling. We are constantly adding new features, so stay tuned."
-      backgroundImage="/images/feature-box-3.svg" />
+      backgroundImage="/images/feature-box-3.png" />
   </div>
 </template>
 
