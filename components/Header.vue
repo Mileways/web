94a8e5be878e94ab08b1aa3f006518ac05e423df
manueljenni <manuelnoahjenni@outlook.com>
2024-09-22 22:22:04 +0200

    Fix header

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,18 @@
 <template>
-  <header class="py-5 flex items-center bg-primary text-white text-lg">
+  <header
+    class="py-5 flex items-center bg-primary text-white text-lg">
     <div class="container w-full mx-auto flex items-center">
       <nuxt-link to="/">
-        <img
-          src="/images/logo_text.svg"
-          alt="Mileways Logo"
-          class="h-4 lg:h-auto"
-        >
+        <img src="/images/logo_text.svg" alt="Mileways Logo"
+          class="h-4 lg:h-auto">
       </nuxt-link>
 
       <nav class="ml-auto">
         <ul class="flex">
           <li class="mx-2 lg:mx-5">
-            <a :href="appStoreUrl">Download</a>
+            <a :href="appStoreUrl" target="_blank"
+              rel="noopener noreferrer"
+              class="cursor-pointer">Download</a>
           </li>
 
           <li class="mx-2 lg:mx-5">
